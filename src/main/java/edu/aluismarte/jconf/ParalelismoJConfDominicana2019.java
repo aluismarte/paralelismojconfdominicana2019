d2ab9295253eb31f38547320c9c7cba957a5da0d
Aluis Marte <aluismarte@gmail.com>
2019-06-17 00:35:49 -0400

    Creando elementos mas actualizados.

--- src/main/java/edu/aluismarte/jconf/ParalelismoJConfDominicana2019.java
+++ src/main/java/edu/aluismarte/jconf/ParalelismoJConfDominicana2019.java
@@ -6,6 +6,7 @@ package edu.aluismarte.jconf;
 public class ParalelismoJConfDominicana2019 {
 
     public static void main(String[] args) {
+        System.out.println("Hola JConf Dominicana 2019!");
         // Tengo un gerente con unas cargas de trabajo que va a llamar de una DB y los repartirá con unos invididuos
     }
 }
